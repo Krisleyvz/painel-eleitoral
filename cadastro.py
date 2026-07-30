0e1fbf0522d14c647472ea74f73142fb527d1a2c
Krisleyvz <krisleyvz77@gmail.com>
2026-07-30 11:33:43 -0500

    Change logo image in cadastro.py

--- cadastro.py
+++ cadastro.py
@@ -44,7 +44,7 @@ st.markdown("""
 col_logo1, col_logo2, col_logo3 = st.columns([1, 2, 1])
 with col_logo2:
     try:
-        st.image("IMG_6009.PNG", use_container_width=True)
+        st.image("IMG_6008.PNG", use_container_width=True)
     except:
         st.title("📝 Cadastro de Rua")
 
